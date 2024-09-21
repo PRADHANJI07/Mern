bf2a7c3f389e25f050f42d548dbcdaee7933f4f3
Bhabesh Pradhan <bhabeshchandrapradhan@gcekbpatna.ac.in>
2024-09-21 18:41:07 +0530

    learning mern

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 4000;
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post("/api/v1/login", (req, res) => {
+  res.send(`<h1>Congratulations Mr. ${req.body.name}</h1>`);
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Server is working on port: ${port}`);
+});
